2f4d8f2f2a55921e04f6c24183d531250e7953c7
eSing10 <ea_singer@live.com>
2018-05-31 20:56:55 -0500

    C:/Program Files/Git/home in about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -71,7 +71,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/') }}">Home</a>
+                        <a href="{{ url('/home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
